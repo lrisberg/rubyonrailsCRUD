ee01b66f3796be420a7d56f0ced5252ec3b7741d
Lauren Risberg <lauren.risberg@gmail.com>
2017-04-27 18:24:14 -0600

    add comments feature to messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170427172725) do
+ActiveRecord::Schema.define(version: 20170428001656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "message_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["message_id"], name: "index_comments_on_message_id", using: :btree
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 20170427172725) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "messages"
 end
